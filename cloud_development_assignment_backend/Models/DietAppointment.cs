bc7556c8ab0d241c1ef85dad5450994ce776180b
DesireeTan8 <129257340+DesireeTan8@users.noreply.github.com>
2025-06-26 11:03:31 +0800

    dietician controller. Model: dietTip, DietAppointment, DietPlan

---
+++ cloud_development_assignment_backend/Models/DietAppointment.cs
@@ -0,0 +1,11 @@
+﻿namespace cloud_development_assignment_backend.Models
+{
+    public class DietAppointment
+    {
+        public string Id { get; set; }
+        public string PatientId { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string Notes { get; set; }
+    }
+}
